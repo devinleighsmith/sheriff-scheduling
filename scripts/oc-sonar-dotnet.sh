c3e925649bad0f71faf9d50c3048b29588407d51
Devin Smith <devinleighsmith@gmail.com>
2020-09-22 16:31:14 -0700

    fix param format.

--- scripts/oc-sonar-dotnet.sh
+++ scripts/oc-sonar-dotnet.sh
@@ -57,7 +57,7 @@ CMD_SONAR_BEGIN="dotnet sonarscanner begin \
   -n:'${SONAR_PROJECT_NAME}' \
   -d:sonar.host.url='${SONAR_URL}' \
   -d:sonar.cs.opencover.reportsPaths='${COVERAGE_DIR}/coverage.opencover.xml' \
-  -Dsonar.projectBaseDir='${WORKSPACE}' \
+  -d:sonar.projectBaseDir='${WORKSPACE}' \
   -d:sonar.login='${SONAR_TOKEN}'"
 
 # Build & Test
